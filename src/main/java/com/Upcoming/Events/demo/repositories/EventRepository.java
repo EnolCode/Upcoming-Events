9ddc4b7c491fc319dd0eee1e582e334a126740d6
Enol-Igareta <enol1code@gmail.com>
2023-03-15 21:10:49 +0100

    pull

--- src/main/java/com/Upcoming/Events/demo/repositories/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Upcoming.Events.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Upcoming.Events.demo.models.Event;
-
-public interface EventRepository extends JpaRepository <Event, Long> {
-    
-}
--- src/main/java/com/Upcoming/Events/demo/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Upcoming.Events.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Upcoming.Events.demo.models.User;
-
-public interface UserRepository extends JpaRepository <User, Long> {
-    
-}
